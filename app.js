bceba36cadceb8ef48a2545043ac1be208f9d80c
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-08-30 12:24:04 +0200

    add click function for button and number validate function

--- app.js
+++ app.js
@@ -3,10 +3,11 @@
 var ravScoreElement = document.getElementById('raw-score-input-value');
 var populationMeanElement = document.getElementById('population-mean-input-value');
 var standardDeviationElement = document.getElementById('standard-deviation-input-value');
+var clickButtonElement = document.getElementById('click-button');
 
 // take value and accept only numbers
 var getValues = function () {
-
+    console.log('getValues()');
     return {
         rawScore : Number(ravScoreElement.value),
         populationMean : Number(populationMeanElement.value),
@@ -16,3 +17,28 @@ var getValues = function () {
 };
 
 // validate if only numbers are put
+
+var validate = function (inputData) {
+    console.log('validate()');
+    if (inputData.ravScoreElement <=0 || inputData.populationMeanElement <= 0 || inputData.standardDeviationElement <= 0) {
+        console.log('There is a number less than zero');
+        return false
+    }
+    else {
+        console.log('Numbers are fine');
+        return true
+    }
+
+};
+
+// calculate
+
+
+// final run
+
+// button click
+clickButtonElement.addEventListener('click', function () {
+    console.log('kliknul sem na button')
+    //put here finalRun function - calculate
+});
+
